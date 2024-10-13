3fd8945a018bb2ca6cfeb8ecfd0c0fa03bbf635e
Philip osir Otieno <philiposir2@gmail.com>
2024-10-13 10:36:48 +0000

    update

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from models import db, Article, User
 
 app = Flask(__name__)
+
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
@@ -22,13 +24,23 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = Article.query.all()
+    return jsonify([article.to_dict() for article in articles])
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views', 0)
+    session['page_views'] += 1
+    
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+    
+    article = Article.query.get(id)
+    if article:
+        return jsonify(article.to_dict())
+    else:
+        return jsonify({'message': 'Article not found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555)
